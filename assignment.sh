3702f1532dbe1ba20ab1bd3469a93bb2253c361a
email_not_provided <s2172876@bioinfmsc5.bio.ed.ac.uk>
2021-10-08 22:25:51 +0100

    samTobam

--- assignment.sh
+++ assignment.sh
@@ -1,32 +1,39 @@
 # 1.Assign all files needed for analysis
 # 1.1.Assign raw fastq file path
-fq_path='/localdisk/data/BPSM/AY21/fastq'
+#fq_path='/localdisk/data/BPSM/AY21/fastq'
+fq_path='/localdisk/home/s2172876/Assignment1/rawfastq'
 # 1.2.Genome file And Bed file
 fa_file='/localdisk/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz'
 bed_file='/localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed'
-#dir=$(ls -l $fq_path | awk '{FS=" ";  {print $9;}}' | sort | grep "fq.gz")
-# file_name=$(ls -l /localdisk/data/BPSM/AY21/fastq | awk '{FS=" ";  {print $9;}}' | sort | grep "fq.gz")
-# count=$(ls -l /localdisk/data/BPSM/AY21/fastq | awk '{FS=" ";  {print $9;}}' | sort | grep "fq.gz" | wc -l)
-# echo $(ls -l $fq_path | awk '{FS=" ";  {print $9;}}' | sort | grep "fq.gz" | awk '{print(substr($1,1,13))}' | sort | uniq) 
-# ls -l $fq_path | awk '{FS=" ";  {print $9;}}' | sort | grep "fq.gz" | awk '{print(substr($1,1,13))}' | sort | uniq
+ls -l $fq_path | awk '{FS=" ";  {print $9;}}' | sort | grep "fq.gz" | awk '{print(substr($1,1,13))}' | sort | uniq >./sampleid.txt
 
 # 2.Perpreration for mapping
-mkdir genome
-cp $fa_file ./genome
-ls ./genome/ | gzip -d 
-hisat2-build ./genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta ./genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome
-
+# mkdir genome
+# cp $fa_file ./genome/Genome.fasta.gz
+# gzip -d ./genome/Genome.fasta.gz
+# hisat2-build ./genome/Genome.fasta ./genome/Genome
+index_path='./genome/Genome'
 
 # 3.
-for file in $fq_path/*.fq.gz
+
+cat ${PWD}/sampleid.txt | while read id
 do
-  	echo ${file:0:45}_1.fq.gz
-    # Perform quality control
+    # 3.1 Perform quality control
     ## -t thread: 5
-    ## output to fastqc folder  
-    fastqc ${file:0:45}_1.fq.gz ${file:0:45}_2.fq.gz -t 5 -o ./fastqc/
+    ## output to fastqc folder
+    fastqc ${fq_path}/${id}_1.fq.gz ${fq_path}/${id}_2.fq.gz -t 5 -o ${PWD}/fastqc/
+    # 3.2 Perform reads alignment
+    ## -p thread: 5
+    ## sam file output to map folder
+    ## log message redirects to map folder and save with filename mapeval(mapping-evaluation)
+    hisat2 -p 5 -x $index_path -1 ${fq_path}/${id}_1.fq.gz -2 ${fq_path}/${id}_2.fq.gz -S ./map/${id}.sam 2> "./map/${id}.mapeval" 
+    # 3.3 sam to bam
+    samtools sort -@ 1 -o ./map/${id}.sam ./map/${id}.bam
 done
 
+
+# hisat2 -x ./genome/Genome -1 /localdisk/home/s2172876/Assignment1/rawfastq/100k.C1-1-501_1.fq.gz \
+# -2 /localdisk/home/s2172876/Assignment1/rawfastq/100k.C1-1-501_2.fq.gz -S ./map/100k.C1-1-501.sam
 # count=$(ls -l $fq_path | awk '{FS=" ";  {print $9;}}' | grep "fq.gz" | wc -l)
 # #echo $dir
 # for ((i=1;i<=count;i=i+2))
