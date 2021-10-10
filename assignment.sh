52f820e68a7ada675151a00757ff9eccb81e1d84
B199735 <email_not_provided>
2021-10-10 16:40:28 +0100

    Sample Grouping

--- assignment.sh
+++ assignment.sh
@@ -14,7 +14,9 @@ gzip -d ./genome/Genome.fasta.gz
 hisat2-build ./genome/Genome.fasta ./genome/Genome
 index_path='./genome/Genome'
 
-# 3.
+# 3. fastq to bam(.bai)
+## Using a loop to analysis each pair of fastq data
+## Using hisat2 to align reads, Using samtools to generate bam files and its index
 mkdir -p map
 mkdir -p fastqc
 cat ${PWD}/sampleid.txt | while read id
@@ -39,10 +41,31 @@ mkdir -p count
 bedtools multicov -bams ./map/*.bam -bed /localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed > count.txt
 ## match sample id to count.txt
 ### extract bam name in order
-ls ./map/*.bam | cut -d "/" -f 3 | cut -d "." -f 1,2 | awk '{printf "%s\t",$1}' > colname
+ls ./map/*.bam | cut -d "/" -f 3 | cut -d "." -f 2 | awk '{printf "%s\t",$1}' > colname
 ### insert colname for first five columns 
 sed 's/^/id\tstart\tend\tlocation\tdescription\t/' colname > expression.txt
 cat count.txt >> expression.txt
 
 
 #5. Statics Mean Calculation
+cp /localdisk/data/BPSM/AY21/fastq/100k.fqfiles .
+## extract 2,4,5 colnames to identity group(samples in a group identical in 2_4_5 row)
+cut 100k.fqfiles -f2,4,5 | sed 's/\t/_/g' | paste 100k.fqfiles - | cut - -f1,8 | sort >> group
+## extract expression matrix
+cut -f 6- expression.txt > count.txt
+## expression matrix transpose in order to merge with grouping information
+awk '{for(i=1;i<=NF;i++){a[FNR,i]=$i}}END{for(i=1;i<=NF;i++){for(j=1;j<=FNR;j++){printf a[j,i]"\t"}print ""}}' count.txt > Texpression.txt
+## merging expression matrix and grouping files
+IFS=$'\t'
+join -j 1 group Texpression.txt > expression_group.txt
+## remove processing files
+rm group count.txt Texpression.txt
+## sort files with grouping column to make sure samples in sample group list adjacent
+sed 's/ /\t/g' expression_group.txt | sort -k2,2 > Texpression.txt
+## transposed expression matrix transpose
+awk '{for(i=1;i<=NF;i++){a[FNR,i]=$i}}END{for(i=1;i<=NF;i++){for(j=1;j<=FNR;j++){printf a[j,i]"\t"}print ""}}' Texpression.txt > expression_group.txt
+## paste sample with gene descrpition
+cut -f 1,2,3,4,5 expression.txt > output.txt
+sed -i "1a\group\tgroup\tgroup\tgroup\tgroup" output.txt
+paste output.txt expression_group.txt > test.txt
+
