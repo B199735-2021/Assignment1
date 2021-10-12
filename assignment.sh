7be228dc9daf3136ec466d35ad5f57912d547daa
B199735 <email_not_provided>
2021-10-12 22:23:10 +0100

    Calculation of Sample Mean (save to separate files)

--- assignment.sh
+++ assignment.sh
@@ -5,6 +5,7 @@ fq_path='/localdisk/data/BPSM/AY21/fastq'
 # 1.2.Genome file And Bed file
 fa_file='/localdisk/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz'
 bed_file='/localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed'
+dir=$(pwd)
 ls -l $fq_path | awk '{FS=" ";  {print $9;}}' | sort | grep "fq.gz" | awk '{print(substr($1,1,13))}' | sort | uniq >./sampleid.txt
 
 # 2.Perpreration for mapping
@@ -31,58 +32,79 @@ do
     ## log message redirects to map folder and save with filename mapeval(mapping-evaluation)
     hisat2 -p 5 -x $index_path -1 ${fq_path}/${id}_1.fq.gz -2 ${fq_path}/${id}_2.fq.gz -S ./map/${id}.sam 2> "./map/${id}.mapeval" 
     # 3.3 sam to bam
-    samtools sort -@ 5 ./map/${id}.sam -o ./map/${id}.bam
+    samtools sort -@ 5 ./map/${id}.sam -o ./map/${id:5:8}.bam
     # 3.4 bam index
-    samtools index ./map/${id}.bam ./map/${id}.bam.bai
+    samtools index ./map/${id:5:8}.bam ./map/${id:5:8}.bam.bai
 done
 
-#4. Generate Count data
-mkdir -p count
-bedtools multicov -bams ./map/*.bam -bed /localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed > count.txt
-## match sample id to count.txt
-### extract bam name in order
-ls ./map/*.bam | cut -d "/" -f 3 | cut -d "." -f 2 | awk '{printf "%s\t",$1}' > colname
-### insert colname for first five columns 
-sed 's/^/id\tstart\tend\tlocation\tdescription\t/' colname > expression.txt
-cat count.txt >> expression.txt
+# #4. Generate Count data
+# mkdir -p count
+# bedtools multicov -bams ./map/*.bam -bed /localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed > count.txt
+# ## match sample id to count.txt
+# ### extract bam name in order
+# ls ./map/*.bam | cut -d "/" -f 3 | cut -d "." -f 2 | awk '{printf "%s\t",$1}' > colname
+# ### insert colname for first five columns 
+# sed 's/^/id\tstart\tend\tlocation\tdescription\t/' colname > expression.txt
+# cat count.txt >> expression.txt
 
 
-#5. Statics Mean Calculation
-cp /localdisk/data/BPSM/AY21/fastq/100k.fqfiles .
-## extract 2,4,5 colnames to identity group(samples in a group identical in 2_4_5 row)
-cut 100k.fqfiles -f2,4,5 | sed 's/\t/_/g' | paste 100k.fqfiles - | cut - -f1,8 | sort >> group
-## extract expression matrix
-cut -f 6- expression.txt > count.txt
-## expression matrix transpose in order to merge with grouping information
-awk '{for(i=1;i<=NF;i++){a[FNR,i]=$i}}END{for(i=1;i<=NF;i++){for(j=1;j<=FNR;j++){printf a[j,i]"\t"}print ""}}' count.txt > Texpression.txt
-## merging expression matrix and grouping files
-IFS=$'\t'
-join -j 1 group Texpression.txt > expression_group.txt
-## remove processing files
-rm count.txt Texpression.txt
-## sort files with grouping column to make sure samples in sample group list adjacent
-sed 's/ /\t/g' expression_group.txt | sort -k2,2 > Texpression.txt
-## transposed expression matrix transpose
-awk '{for(i=1;i<=NF;i++){a[FNR,i]=$i}}END{for(i=1;i<=NF;i++){for(j=1;j<=FNR;j++){printf a[j,i]"\t"}print ""}}' Texpression.txt > expression_group.txt
-## paste sample with gene descrpition
-cut -f 1,2,3,4,5 expression.txt > output.txt
-sed -i "1a\group\tgroup\tgroup\tgroup\tgroup" output.txt
-paste output.txt expression_group.txt > test.txt
+# #5. Statics Mean Calculation
+# cp /localdisk/data/BPSM/AY21/fastq/100k.fqfiles .
+# ## extract 2,4,5 colnames to identity group(samples in a group identical in 2_4_5 row)
+# cut 100k.fqfiles -f2,4,5 | sed 's/\t/_/g' | paste 100k.fqfiles - | cut - -f1,8 | sort >> group
+# ## extract expression matrix
+# cut -f 6- expression.txt > count.txt
+# ## expression matrix transpose in order to merge with grouping information
+# awk '{for(i=1;i<=NF;i++){a[FNR,i]=$i}}END{for(i=1;i<=NF;i++){for(j=1;j<=FNR;j++){printf a[j,i]"\t"}print ""}}' count.txt > Texpression.txt
+# ## merging expression matrix and grouping files
+# IFS=$'\t'
+# join -j 1 group Texpression.txt > expression_group.txt
+# ## remove processing files
+# rm count.txt Texpression.txt
+# ## sort files with grouping column to make sure samples in sample group list adjacent
+# sed 's/ /\t/g' expression_group.txt | sort -k2,2 > Texpression.txt
+# ## transposed expression matrix transpose
+# awk '{for(i=1;i<=NF;i++){a[FNR,i]=$i}}END{for(i=1;i<=NF;i++){for(j=1;j<=FNR;j++){printf a[j,i]"\t"}print ""}}' Texpression.txt > expression_group.txt
+# ## paste sample with gene descrpition
+# cut -f 1,2,3,4,5 expression.txt > output.txt
+# sed -i "1a\group\tgroup\tgroup\tgroup\tgroup" output.txt
+# paste output.txt expression_group.txt > test.txt
 
 # sed "1d" 100k.fqfiles | cut -f2,3,4,5,6 | sort -k1,1 -k3,3 -k4,4 
-
+###分组文件
 # sed "1d" 100k.fqfiles | cut -f2,4,5 | sed 's/\t/_/g' > condition.txt
 # sed "1d" 100k.fqfiles | nl - | cut -f1 | paste condition.txt - | \
 # awk '{array[$1] = array[$1] ? array[$1] " "$2 : $2} {for (i in array) print i,array[i]}' > grouping_file.txt
-IFS=$'\t'
-mkdir -p mean
-i=6
-sample_number=50
-while [ $i -le $sample_number ]
-do
-  cut -f "$i" test.txt | less -S | sed -n '2,2p' > "./mean/$(sed -n '2,2p' test.txt | cut -f $i).txt"
-  sed '1,2d' test.txt | cut -f "$i","$((i+1))","$((i+2))" | awk '{sum=0;for(i=1;i<=NF;i++)sum+=$i;print sum/3}' >> "./mean/$(sed -n '2,2p' test.txt | cut -f $i).txt"
-  let i=i+3
-done
 
-paste ./mean
+# IFS=$'\t'
+# mkdir -p mean
+# i=6
+# sample_number=50
+# while [ $i -le $sample_number ]
+# do
+#   #sample name
+#   cut -f "$i" test.txt | less -S | sed -n '2,2p' > "./mean/$(sed -n '2,2p' test.txt | cut -f $i).txt"
+#   sed '1,2d' test.txt | cut -f "$i","$((i+1))","$((i+2))" | awk '{sum=0;for(i=1;i<=NF;i++)sum+=$i;print sum/3}' >> "./mean/$(sed -n '2,2p' test.txt | cut -f $i).txt"
+#   let i=i+3
+# done
+
+###分组文件,第二列要文件名
+mkdir -p group_count
+cd ./map
+cp /localdisk/data/BPSM/AY21/fastq/100k.fqfiles .
+sed "1d" 100k.fqfiles | cut -f2,4,5 | sed 's/\t/_/g' > condition.txt
+sed "1d" 100k.fqfiles | cut -f1 | paste condition.txt - | \
+awk  '{a[$1]=a[$1]?a[$1]","$2:$2}END{for (i in a) print i,a[i]}' > grouping_file.txt
+
+for i in $(seq 1 $(cat grouping_file.txt | wc -l))
+do
+s=$(sed -n "${i}p" /localdisk/home/s2172876/Assignment1/planb/grouping_file.txt | awk '{print $2}')
+s=${s//,/.bam }
+bedtools multicov -bams ${s//,/ }.bam -bed $bed_file > $dir/group_count/$(sed -n "${i}p" grouping_file.txt | awk '{print $1}').txt
+## match sample id to count.txt
+#echo $(sed -n "${i}p" grouping_file.txt | awk '{print $2}' | cut -d "," -f $j) #cat ./count/$x
+done
+cd ..
+#mean https://stackoverflow.com/questions/31645668/average-of-multiple-files-in-shell
+
+
